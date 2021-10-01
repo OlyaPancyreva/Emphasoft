e60b9836973e9fbb2e2343bffe5df8e304ff017e
O'Lik <55744134+OlyaPancyreva@users.noreply.github.com>
2021-10-01 14:38:35 +0300

    fix

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Users
 from .serializers import *
 
 
 class UserView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, ]
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+
